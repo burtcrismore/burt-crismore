b471668c5bdb4c2147741106e174ee04ab2d4d79
Burt Crismore <burtcrismore@Burts-MacBook-Pro-2.local>
2025-08-15 12:30:25 -0700

    Added robots.txt, sitemap, purgecss, and general authoring updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,36 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
 	devToolbar: {
 		enabled: false
 	},
+	build: {
+		inlineStylesheets: "never",
+	},
 	site: 'https://www.burtcrismore.com',
-	integrations: [mdx()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		purgecss(),
+	],
+	vite: {
+		build: {
+			cssCodeSplit: false,
+			rollupOptions: {
+				output: {
+					assetFileNames: (assetInfo) => {
+						  if (assetInfo.name?.endsWith('.css')) {
+								return 'assets/global.[hash][extname]';
+							}
+							return 'assets/[name].[hash][extname]';
+					}
+				}
+			}
+		}
+	}
 });
--- src/data/recommendations.js
+++ src/data/recommendations.js
@@ -64,5 +64,104 @@ export default [
 		},
 		person: "Robert Torres",
 		title: "UX Leader & Innovator"
-	}
+	},
+	{
+		id: "seth",
+		variant: "recommendations",
+		quote: "His collaborative spirit and straightforward approach consistently made him an effective colleague.",
+		link: {
+			url: "https://www.linkedin.com/in/burtcrismore/",
+			target: "_blank"
+		},
+		person: "Seth Lilavivat",
+		title: "Staff eBusiness Analyst"
+	},
+	{
+		id: "meenal",
+		variant: "recommendations",
+		quote: "His dedication and meticulous approach have consistently ensured that our projects meet the highest standards of quality.",
+		link: {
+			url: "https://www.linkedin.com/in/burtcrismore/",
+			target: "_blank"
+		},
+		person: "Meenal Naik",
+		title: "Associate Director, IT"
+	},
+	{
+		id: "john",
+		variant: "recommendations",
+		quote: "He managed complex tasks, worked well with stakeholders, and resolved project risks, all of which were key to the project's success.",
+		link: {
+			url: "https://www.linkedin.com/in/burtcrismore/",
+			target: "_blank"
+		},
+		person: "John Haertjens",
+		title: "Associate Director, IT"
+	},
+	{
+		id: "esther",
+		variant: "recommendations",
+		quote: "His attention to detail and ability to liaison between design and development makes him an asset to any team.",
+		link: {
+			url: "https://www.linkedin.com/in/burtcrismore/",
+			target: "_blank"
+		},
+		person: "Esther Luc",
+		title: "Staff Experience Designer"
+	},
+	{
+		id: "andrea",
+		variant: "recommendations",
+		quote: "Burt's attention to detail and ability to stitch together project details from strategy to design to end review were remarkable.",
+		link: {
+			url: "https://www.linkedin.com/in/burtcrismore/",
+			target: "_blank"
+		},
+		person: "Andrea Misiner",
+		title: "Assoc. Director, Marketing"
+	},
+	{
+		id: "riccardo",
+		variant: "recommendations",
+		quote: "Burt is a dependable team player with excellent communication skills and a rare ability to align cross-functional teams spanning design, development, and strategy.",
+		link: {
+			url: "https://www.linkedin.com/in/burtcrismore/",
+			target: "_blank"
+		},
+		person: "Riccardo Caruso",
+		title: "VP, Digital/eCommerce"
+	},
+	{
+		id: "leyla",
+		variant: "recommendations",
+		quote: "Burt set cross-functional processes in motion that elevated our entire team's performance. He is a master of his craft. Any team would be lucky to have him",
+		link: {
+			url: "https://www.linkedin.com/in/burtcrismore/",
+			target: "_blank"
+		},
+		person: "Leyla Badillo",
+		title: "Manager, Web Dev"
+	},
+	{
+		id: "mayur",
+		variant: "recommendations",
+		quote: "With a remarkable attention to detail and a deep understanding of process improvement, Burt consistently elevates the quality, usability, and efficiency of our work.",
+		link: {
+			url: "https://www.linkedin.com/in/burtcrismore/",
+			target: "_blank"
+		},
+		person: "Mayur Madawar",
+		title: "Sr Staff Developer"
+	},
+	{
+		id: "eddie",
+		variant: "recommendations",
+		quote: "Burt is also a great bridge between creative and development, with his ability to craft thoughtful, well-articulated requirements that ensure project success.",
+		link: {
+			url: "https://www.linkedin.com/in/burtcrismore/",
+			target: "_blank"
+		},
+		person: "Eddie Valenzuela",
+		title: "Staff Product Designer"
+	},
 ]
